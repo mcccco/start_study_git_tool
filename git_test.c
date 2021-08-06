79f4d55b71d979732ab8ea2e681b1ed570ed1d41
maoyi <maoyi@jx.cn>
2021-08-06 11:00:29 +0800

    增加命令与使用说明

--- git_test.c
+++ git_test.c
@@ -2,7 +2,12 @@
 
 int main(){
     printf("hello git\n");
-    int b = 0;
-    int a = 0;
+    printf("git init\n"); //创建并初始化git仓库
+    printf("git branch\n"); //查看所有分支
+    printf("git branch $name\n"); //创建新的分支，$name为新分支的名称
+    printf("git add $file\n");  //将工作区修改的文件传入暂存区,$file为文件名
+    printf("git commit -m \"$context\"\n"); //将暂存区的内容提交到版本库，$context为提交说明
+    printf("git checkout $branch"); //切换分支，$branch为要切换的分支名
+
     return 0;
 }
